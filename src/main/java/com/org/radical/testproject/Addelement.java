c385ebf5d44a8e6e64a77713191d519f586f6cec
Ritika <nasery.ritika@gmail.com>
2024-02-20 23:04:26 +0800

    Testing File added

--- src/main/java/com/org/radical/testproject/Addelement.java
+++ src/main/java/com/org/radical/testproject/Addelement.java
@@ -1,24 +1,22 @@
 package com.org.radical.testproject;
+
 import java.util.ArrayList;
 
 public class Addelement {
-	
-	    
-	  public static void main(String[] args) {
-	      
-	    ArrayList<String> students = new ArrayList<String>();
-	    
-	    
-	    students.add("John");
-	    students.add("Jane");
-	    students.add("Doe");
-	    students.add("Doe");
-	    
-	    
-	    
-	    System.out.println(students);
-	    // [John, Jane, Doe]
-	    
-	  }
-	}
 
+	public static void main(String[] args) {
+
+		ArrayList<String> students = new ArrayList<String>();
+
+		students.add("John");
+		students.add("Jane");
+		students.add("Doe");
+		students.add("Doe");
+		students.add("Ritika");
+		students.add("Doe");
+
+		System.out.println(students);
+		// [John, Jane, Doe]
+
+	}
+}
